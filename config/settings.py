d3e313cbd3e1cc49cb4ee3b4b5d2187fdb770d74
robochi-work <robochi.work@gmail.com>
2025-09-10 17:30:11 +0300

    fix: обновлены settings.py и base.html

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "замени-на-настоящий-в-продакшене")
 
