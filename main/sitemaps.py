729b53338e92207458d825bc92c46355088f410f
robochi-work <robochi.work@gmail.com>
2025-09-25 18:07:40 +0300

    Feature/need telegram index (#16)
    * Fix: убраны редиректы и скрытый H1, обновлены стили, добавлен robots.txt
    
    * Update .gitignore: exclude run_local.ps1
    
    * Feat: make need_telegram indexable and add to sitemap

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -10,3 +10,16 @@ class StaticViewSitemap(Sitemap):
 
     def location(self, item):
         return reverse(item)
+    
+class NeedTelegramSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.8
+
+    def items(self):
+        return ['need_telegram_employer', 'need_telegram_worker']
+
+    def location(self, item):
+        if item == 'need_telegram_employer':
+            return reverse('need_telegram', args=['employer'])
+        elif item == 'need_telegram_worker':
+            return reverse('need_telegram', args=['worker'])
